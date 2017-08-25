0ee80fdbe42b1c32173bff9ad6729da808b67bab
Witty Chang <wcc1213@gmail.com>
2017-08-25 16:07:59 -0700

    Add styles for homepage component.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,12 @@
-import { ModuleWithProviders }  from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component'
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
