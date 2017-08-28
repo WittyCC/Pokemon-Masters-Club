5ef610576cfc6440314c284b837d3972a69cb642
Witty Chang <wcc1213@gmail.com>
2017-08-28 01:27:09 -0700

    Add functionality to update and delete club members.

--- src/app/edit-member/edit-member.component.ts
+++ src/app/edit-member/edit-member.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-edit-member',
   templateUrl: './edit-member.component.html',
-  styleUrls: ['./edit-member.component.css']
+  styleUrls: ['./edit-member.component.css'],
+  providers: [MemberService]
 })
 export class EditMemberComponent implements OnInit {
 
-  constructor() { }
+  @Input() selectedMember;
+
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
   }
+  beginUpdatingMember(memberToUpdate){
+    this.memberService.updateMember(memberToUpdate);
+  }
 
+  beginDeletingMember(memberToDelete){
+    if(confirm("Are you sure you want to delete this member from the registry?")){
+      this.memberService.deleteMember(memberToDelete);
+    }
+  }
 }
